207c706910a957483cbbdc63a6d64f836ff0c3c4
Devon <devonm@northseattle.edu>
2018-04-13 13:18:15 -0700

    fixed gradle build. Tests are old and should be merged

--- app/src/main/java/com/devon/firstapplication/MainActivity.java
+++ app/src/main/java/com/devon/firstapplication/MainActivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import com.google.firebase.analytics.FirebaseAnalytics;
+//import com.crashlytics.android.Crashlytics;
+//import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView helloWorld;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.helloWorld = findViewById(R.id.hello_world);
