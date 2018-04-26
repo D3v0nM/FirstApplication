5edb6885aad51d0551c1c5d4770fe5fcf2e74ea9
Devon <devonm@northseattle.edu>
2018-04-26 07:55:10 -0700

    Updated all error handling on form. All seems right. MOAR tests!

--- app/src/androidTest/java/com/devon/firstapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/devon/firstapplication/MainActivityTest.java
@@ -2,6 +2,7 @@ package com.devon.firstapplication;
 
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -19,13 +20,14 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainActivityTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> ActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule(MainActivity.class);
 
     @Test
-
     public void menu() {
         onView(withId(R.id.hello_world))
                 .check(matches(withText(R.string.hello)));
@@ -37,6 +39,35 @@ public class MainActivityTest {
 
         onView(withId(R.id.hello_world))
                 .check(matches(withText(R.string.menu1)));
+    }
+
+    @Test
+    public void enterAllErrorsInAllForm(){
+
+        //Leave Name field blank
+        onView(withId(R.id.nameEditText)).check(matches(withText("")));
+
+        //Leave email blank
+        onView(withId(R.id.emailEditText)).check(matches(withText("")));
+
+        //Leave User name blank
+        onView(withId(R.id.userEditText)).check(matches(withText("")));
+
+        //Leave password blank
+        onView(withId(R.id.passEditText)).check(matches(withText("")));
+
+        //Leave job blank
+        onView(withId(R.id.job)).check(matches(withText("")));
+
+        //Verify login/Create button labeled correctly
+        onView(withId(R.id.loginBtn)).check(matches(withText(R.string.login)));
+
+        onView(withId(R.id.loginBtn)).perform(click());
+
+
+
+
+
     }
 
 
